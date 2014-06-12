e2d3ef77e24db9eb24205a5e88a45e19b4a3cc41
Shahab Layeghi <shahab.layeghi@intel.com>
2014-06-12 14:10:22 -0700

    more cleanup

--- documents/LiveCSSDocument.js
+++ documents/LiveCSSDocument.js
@@ -123,7 +123,6 @@ define(function LiveCSSDocumentModule(require, exports, module) {
     LiveCSSDocument.prototype._updateBrowser = function () {
         // TODO Need to add protocol API to replace associated stylesheet at runtime with the current
         // text of the document.
-        //var reloadPromise = CSSAgent.reloadCSSForDocument(this.doc);
         
         this.protocol.evaluate(this.getConnectionIds(), "_LD.reloadCSS(" + JSON.stringify(this.doc.url) + ")");
 
